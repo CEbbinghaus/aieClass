d8fd98b3df3971cfeacee96ee782b4df7f715ef4
Christopher-Robin <jecebbi@gmail.com>
2020-06-24 12:29:38 +1000

    Added Asking for Name

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!\n");
+    printf("Hello World!\nWhat is your Name?\n>");
+    char test[20];
+    scanf("%20s", test);
+    printf("Your Name is: %s", test);
     return 0;
 }
